cdfad7ad127430eed4dc8effa7890acfcc7a0f05
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-12 21:42:30 -0300

    Feat: Menu of options

--- Math.c
+++ Math.c
@@ -2,5 +2,18 @@
 
 int main()
 {
+    printf("===============MENU===============\n");
+    printf("----------O que procuras?---------\n");
+    printf("1- O fatorial de um numero\n");
+    printf("2- O numero hamonico designado por Hn\n");
+    printf("3- O valor de S\n");
+    printf("4- O valor de E\n");
+    printf("5- A raiz de um numero\n");
+    printf("6- A funcao para calcular cosseno pelo polinomio de Mclaurin\n");
+    printf("7- A Funcao para calcular o seno de uma serie\n");
+    printf("8- help (ajuda)\n");
+    printf("0- Sair\n");
+    printf("==================================\n");
+    
     return 0;
 }
